4529a9e7cabc9c5da8791f7fd57bf10922ff625c
Denzel <jdenzelmab@gmail.com>
2024-01-31 14:50:02 -0500

    Added attendance app crispy bootstrap to settings

--- project/project/settings.py
+++ project/project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'attendance.apps.AttendnaceConfig',
+    'crispy_forms',
+    'crispy_bootstrap5'
 ]
 
 MIDDLEWARE = [
