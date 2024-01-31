26de139d3705fd52f1e7ab29ea788bce3817cf9b
Denzel <jdenzelmab@gmail.com>
2024-01-31 15:17:11 -0500

    Fixed typo in settings and navbar items not showing

--- project/attendance/views.py
+++ project/attendance/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return 
+    return render(request, 'attendance/home.html')
--- project/project/settings.py
+++ project/project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'attendance.apps.AttendnaceConfig',
+    'attendance.apps.AttendanceConfig',
     'crispy_forms',
     'crispy_bootstrap5'
 ]
