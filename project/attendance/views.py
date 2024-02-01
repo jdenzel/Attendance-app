62b5667c5e54da5ffadc7c2be1cf20ed7b5ff8f0
Denzel <jdenzelmab@gmail.com>
2024-02-01 17:32:59 -0500

    Created clock out view and added comments to explain code

--- project/attendance/views.py
+++ project/attendance/views.py
@@ -24,8 +24,17 @@ def sign_up(request):
 @login_required(login_url="/login")
 def clock_in(request):
     if request.method == 'POST':
-        time_clock = TimeClock(employee=request.user, role=request.POST.get('role'))
-        time_clock.save()
-        request.session['clocked_in'] = True
-        return redirect('clock_out')
-    return render(request, 'clocked_in.html')
+        time_clock = TimeClock(employee=request.user, role=request.POST.get('role')) # Creates a new TimeClock instance and requests the desired role of the currently logged in user
+        time_clock.save() # Saves the Instance
+        request.session['clocked_in'] = True # Sets clocked_in variable to True to tell indicate the user is clocked in
+        return redirect('clock_out') # Changes view to clock out view
+    return render(request, 'clocked_in.html') # Renders clock in view if method is not POST
+
+@login_required(login_url="/login")
+def clock_out(request):
+    if request.method == 'POST':
+        time_clock = TimeClock.objects.filter(employee=request.user).latest('clock_in_time') # Retrieves the latest TimeClock instance
+        time_clock.save() # Saves the Instance
+        request.session['clocked_in'] = False # Sets clocked_in to False to indicate user is clocked out
+        return redirect('clocked_in') # Changes view to clocked_in view
+    return render(request, 'clocked_out.html') # Renders clocked out view if method is not POST
