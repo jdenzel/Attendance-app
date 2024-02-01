ce8de156f808faae9174a096352e553ed1348496
Denzel <jdenzelmab@gmail.com>
2024-02-01 15:39:47 -0500

    Added role field to TimeClock model

--- project/attendance/models.py
+++ project/attendance/models.py
@@ -3,9 +3,20 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class TimeClock(models.Model):
+    ROLES = [
+        ('scoreboard', 'Scoreboard'),
+        ('paperscorer', 'Paper Scorer'),
+        ('camera', 'Camera Operator'),
+        ('onlinescorer', 'Online Scorer'),
+        ('gamechange', 'Game Changer'),
+        ('subtime', 'Sub timer'),
+
+    ]
+
     employee = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateField(auto_now_add=True)
     clock_in_time = models.DateTimeField(auto_now_add=True)
     clock_out_time = models.DateTimeField(auto_now_add=True)
+    role = models.CharField(choices = ROLES, default='none')
 
     
