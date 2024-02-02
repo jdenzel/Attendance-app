4f67c98bc705fcf46ae7f55c83ee076e7936983d
Denzel <jdenzelmab@gmail.com>
2024-02-01 21:25:55 -0500

    Fixed TimeClock model issue causing time to be incorrect and clock-out view to have errors.

--- project/attendance/models.py
+++ project/attendance/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 # Create your models here.
 
+
 class TimeClock(models.Model):
     ROLES = [
         ('scoreboard', 'Scoreboard'),
@@ -11,8 +12,7 @@ class TimeClock(models.Model):
         ('onlinescorer', 'Online Scorer'),
         ('gamechange', 'Game Changer'),
         ('subtime', 'Sub timer'),
-
-    ]
+        ]
     employee = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateField(auto_now_add=True)
     clock_in_time = models.DateTimeField(auto_now_add=True)
@@ -20,10 +20,8 @@ class TimeClock(models.Model):
     role = models.CharField(choices = ROLES, default='none')
 
     def clock_out(self):
-        self.clock_out_time = timezone.now()
+        self.clock_out_time = timezone.localtime(timezone.now())
         self.save()
 
     def __str__(self):
-        return self.employee + '\n' + self.date + self.clock_in_time + '\n' + self.clock_out_time + '\n' + self.role 
-
-    
+        return self.employee + '\n' + self.date + self.clock_in_time + '\n' + self.clock_out_time + '\n' + self.role
