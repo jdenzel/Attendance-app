0297529ece0acafd2caf6244e1d72264463673ed
Denzel <jdenzelmab@gmail.com>
2024-02-07 18:29:34 -0500

    Completed form for updating timeclock and added a url pattern for it

--- project/attendance/urls.py
+++ project/attendance/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('sign-up', views.sign_up, name='sign_up'),
     path('clock-in', views.clock_in, name='clock_in'),
     path('clock-out', views.clock_out, name='clock_out'),
-    path('timesheet', views.timesheet, name='time_sheet')
+    path('timesheet', views.timesheet, name='time_sheet'),
+    path('timeclock/update/<int:pk>/', views.UpdateTimeClock, name='update_timeclock')
 ]
